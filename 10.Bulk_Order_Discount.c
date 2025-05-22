6f5bcc0e0959aafeaaafc247e40f99ccc929845e
name <@gmail.com>
2025-05-22 21:58:11 +0300

    branch10: second commit

--- 10.Bulk_Order_Discount.c
+++ 10.Bulk_Order_Discount.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int quantity;
+    float pricePerUnit, total;
+
+    printf("Enter quantity: ");
+    scanf("%d", &quantity);
+    printf("Enter price per unit: ");
+    scanf("%f", &pricePerUnit);
+
+    total = pricePerUnit * quantity;
+
+    if (quantity >= 10)
+        total -= total * 0.15;
+
+    printf("Total cost: %.2f\n", total);
+    return 0;
+}
