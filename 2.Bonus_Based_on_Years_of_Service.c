bae872d68bd40babbcdddc7f1692780ccec80587
name <@gmail.com>
2025-05-22 21:39:41 +0300

    branch2: second commit

--- 2.Bonus_Based_on_Years_of_Service.c
+++ 2.Bonus_Based_on_Years_of_Service.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int years;
+    float salary, finalSalary;
+
+    printf("Enter years of service: ");
+    scanf("%d", &years);
+    printf("Enter basic salary: ");
+    scanf("%f", &salary);
+
+    if (years > 5)
+        finalSalary = salary + (salary * 0.05);
+    else
+        finalSalary = salary;
+
+    printf("Final salary: %.2f\n", finalSalary);
+    return 0;
+}
