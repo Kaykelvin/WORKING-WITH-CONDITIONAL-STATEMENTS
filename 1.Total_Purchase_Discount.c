9b7f57be39c671de753c182365f2bdb16a000626
name <@gmail.com>
2025-05-22 21:35:27 +0300

    branch1: second commit

--- 1.Total_Purchase_Discount.c
+++ 1.Total_Purchase_Discount.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float amount, finalAmount;
+    printf("Enter total purchase amount: ");
+    scanf("%f", &amount);
+
+    if (amount > 1000)
+        finalAmount = amount - (amount * 0.10);
+    else
+        finalAmount = amount;
+
+    printf("Final amount: %.2f\n", finalAmount);
+    return 0;
+}
 
