dcb1ec649f020ebd1e7085904414dd3701c66574
name <@gmail.com>
2025-05-22 21:51:28 +0300

    branch7: second commit

--- 7.Withdrawal_Transaction_Fee.c
+++ 7.Withdrawal_Transaction_Fee.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    float amount, netAmount;
+
+    printf("Enter withdrawal amount: ");
+    scanf("%f", &amount);
+
+    if (amount < 500)
+        netAmount = amount - 10;
+    else
+        netAmount = amount;
+
+    printf("Net amount received: %.2f\n", netAmount);
+    return 0;
+}
