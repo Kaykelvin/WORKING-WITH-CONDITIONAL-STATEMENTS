e3233c3e542b4db40f079dd17ef833e000f525a6
name <@gmail.com>
2025-05-22 21:53:15 +0300

    branch8: second commit

--- 8.Electricity_Bill_Calculator.c
+++ 8.Electricity_Bill_Calculator.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int units;
+    float bill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if (units < 200)
+        bill = units * 0.50;
+    else
+        bill = units * 0.75;
+
+    printf("Total bill: %.2f\n", bill);
+    return 0;
+}
