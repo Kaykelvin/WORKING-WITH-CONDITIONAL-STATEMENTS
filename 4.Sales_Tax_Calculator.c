a58e3d2195bdc8c0c58272a2ecd210f2cdae4605
name <@gmail.com>
2025-05-22 21:45:18 +0300

    branch4: second commit

--- 4.Sales_Tax_Calculator.c
+++ 4.Sales_Tax_Calculator.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float amount, taxRate, total;
+
+    printf("Enter purchase amount: ");
+    scanf("%f", &amount);
+
+    taxRate = (amount < 500) ? 0.05 : 0.08;
+    total = amount + (amount * taxRate);
+
+    printf("Total payable amount: %.2f\n", total);
+    return 0;
+}
 
