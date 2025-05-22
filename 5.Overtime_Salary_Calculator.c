208cce3654fab6fc403b966a8822d74af90726cf
name <@gmail.com>
2025-05-22 21:47:31 +0300

    branch5: second commit

--- 5.Overtime_Salary_Calculator.c
+++ 5.Overtime_Salary_Calculator.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    float hours, rate, salary;
+
+    printf("Enter hours worked: ");
+    scanf("%f", &hours);
+    printf("Enter hourly rate: ");
+    scanf("%f", &rate);
+
+    if (hours > 40)
+        salary = (40 * rate) + ((hours - 40) * rate * 1.5);
+    else
+        salary = hours * rate;
+
+    printf("Total salary: %.2f\n", salary);
+    return 0;
+}
